2329c870e315b30ed9eaae1624b464f0a7674fa2
mirantel <magi.mirantel@gmail.com>
2012-12-12 19:19:40 +0200

    main js

--- js/common.js
+++ js/common.js
@@ -1,3 +1,24 @@
 $(document).ready(function() {
+	
+	// lang
+	$('.lang__active').click(function(){
+		$(this).next().fadeIn('fast');
+	});
 
+	$('.lang__drop li').click(function(){
+		var lang = $(this).parent().parent().prev().children('span');
+		var langn = lang.html();
+		lang.html($(this).children().html());
+		$(this).children().html(langn)
+		$(this).parent().parent().fadeOut('fast');
+	});
+
+
+
+	// tabs
+	$('.tabs__nav li').click(function(){
+		if (!($(this).hasClass('active'))) $(this).addClass('active').siblings().removeClass('active')
+			.parents('.tabs').find('.tabs__box').eq($(this).index()).fadeIn(150).siblings('.tabs__box').hide();
+		return false;
+	});
 });
